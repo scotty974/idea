b131bbe396bc22adb90b543c71f7a9ff2a9a936f
scotty974 <fabienethevepro@gmail.com>
2024-01-22 23:26:29 +0100

    style input

--- src/composant/Form/InputWords.tsx
+++ src/composant/Form/InputWords.tsx
@@ -1,5 +1,7 @@
 export default function InputWords() {
+
+    
   return <>
-    <input type="text" name="text" id="text"  className="my-16 py-4 px-2 w-full focus:outline-none text-4xl text-primary font-bold " placeholder="Ecrivez quelque chose...."/>
+    <input type="text" name="text" id="text"  className="my-16 py-4 px-2 w-full focus:outline-none text-4xl text-primary font-bold " autoFocus/>
   </>;
 }
