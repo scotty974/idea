adb4d0ad34ac842be111884b09a5997649fa30ad
scotty974 <fabienethevepro@gmail.com>
2024-01-22 23:35:16 +0100

    add create a word

--- src/composant/Form/InputWords.tsx
+++ src/composant/Form/InputWords.tsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+import Form from "./form.interface";
 export default function InputWords() {
+  const [words, setWords] = useState<Form>();
 
-    
-  return <>
-    <input type="text" name="text" id="text"  className="my-16 py-4 px-2 w-full focus:outline-none text-4xl text-primary font-bold " autoFocus/>
-  </>;
+  const handleChange = (e: any) => {
+    setWords({ value: e.target.value });
+  };
+
+  const handleSubmit = (event:any) => {
+    event.preventDefault()
+    setWords({value : ''})
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="text"
+          id="text"
+          className="my-16 py-4 px-2 w-full focus:outline-none text-4xl text-primary font-bold "
+          autoFocus
+          value={words?.value}
+          onChange={handleChange}
+        />
+      </form>
+    </>
+  );
 }
---
+++ src/composant/Form/form.interface.tsx
@@ -0,0 +1,3 @@
+export default interface Form {
+    value : string
+}
