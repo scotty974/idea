45df8cc8e2900042c0b1b5b456561bc14533d8c8
scotty974 <fabienethevepro@gmail.com>
2024-01-23 23:27:14 +0100

    add text

--- src/composant/HeaderComposant/Header.tsx
+++ src/composant/HeaderComposant/Header.tsx
@@ -1,10 +1,22 @@
-import { Menu, MenuButton, MenuList } from "@chakra-ui/react";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverArrow,
+  PopoverCloseButton,
+} from "@chakra-ui/react";
 import data from "../../theme.js";
 import { useState, useEffect } from "react";
 import { useColorContext } from "../../context/ColorContext.js";
+import { HelpCircle } from "lucide-react";
 
 export default function Header() {
-  const {currentColor, changeColor} = useColorContext()
+  const { changeColor } = useColorContext();
   const [textClass, setTextClass] = useState("");
   const [hoverBgClassName, setHoverBgClassName] = useState("");
   const [hoverTextClassName, setHoverTextClassName] = useState("");
@@ -46,24 +58,43 @@ export default function Header() {
       <header className="my-2">
         <nav className={`flex items-center justify-between ${textClass}`}>
           <h1 className="font-bold text-4xl">Logo</h1>
-          <Menu>
-            <MenuButton
-              className={`p-2 rounded-full px-4 font-semibold ${hoverBgClassName} ${hoverTextClassName} ${textClass}`}
-            >
-              Theme
-            </MenuButton>
-            <MenuList>
-              <div className="flex px-2 justify-start">
-                {theme.map((item, index) => (
-                  <span
-                    className={`w-5 h-5 rounded-full mr-2 hover:cursor-pointer ${item.background}`}
-                    onClick={() => selectedColor(item)}
-                    key={index}
-                  ></span>
-                ))}
-              </div>
-            </MenuList>
-          </Menu>
+          <div className="flex items-center">
+            <Menu>
+              <MenuButton
+                className={`p-2 rounded-full px-4 font-semibold ${hoverBgClassName} ${hoverTextClassName} ${textClass} mx-2`}
+              >
+                Theme
+              </MenuButton>
+              <MenuList>
+                <div className="flex px-2 justify-start">
+                  {theme.map((item, index) => (
+                    <span
+                      className={`w-5 h-5 rounded-full mr-2 hover:cursor-pointer ${item.background}`}
+                      onClick={() => selectedColor(item)}
+                      key={index}
+                    ></span>
+                  ))}
+                </div>
+              </MenuList>
+            </Menu>
+
+            <Popover>
+              <PopoverTrigger>
+                <HelpCircle className="hover:cursor-pointer" />
+              </PopoverTrigger>
+              <PopoverContent className="text-white">
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Information !</PopoverHeader>
+                <PopoverBody >
+                  Libérez votre créativité sur notre site ! Écrivez librement,
+                  surmontez le syndrome de la feuille blanche, et personnalisez
+                  votre expérience avec des thèmes interactifs. Exprimez-vous
+                  dès maintenant !
+                </PopoverBody>
+              </PopoverContent>
+            </Popover>
+          </div>
         </nav>
       </header>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ColorProvider } from "./context/ColorContext.tsx";
-
+import { ChakraProvider } from "@chakra-ui/react";
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ChakraProvider>
     <ColorProvider>
-    <App />
+      <App />
     </ColorProvider>
+  </ChakraProvider>
 );
