d861015fe53fcbcd08f607b53abe8918c4f19748
scotty974 <fabienethevepro@gmail.com>
2024-01-23 22:36:57 +0100

    fix bug with themes

--- src/composant/HeaderComposant/Header.tsx
+++ src/composant/HeaderComposant/Header.tsx
@@ -1,45 +1,59 @@
-import { Menu, MenuButton, MenuList, useColorMode } from "@chakra-ui/react";
-import { ChevronDown } from "lucide-react";
+import { Menu, MenuButton, MenuList } from "@chakra-ui/react";
 import data from "../../theme.js";
 import { useState } from "react";
 import { useEffect } from "react";
 export default function Header() {
-  const { toggleColorMode, colorMode } = useColorMode();
-  const textClassName = colorMode === "light" ? "text-primary" : "text-white";
-  const hoverBgClassName = colorMode === "light" ? "hover:bg-secondary" : "hover:bg-slate-900";
-  const hoverTextClassName = colorMode === "light" ? "hover:text-three" : "hover:text-white"
+  
+  const [textClass, setTextClass] = useState("");
+  const [hoverBgClassName, setHoverBgClassName] = useState("");
+  const [hoverTextClassName, sethoverTextClassName] = useState("");
 
-  const [theme, setTheme] = useState([])
-  const themes = data.themes
+  const [theme, setTheme] = useState([]);
+  const themes = data.themes;
 
-  const handleTheme = () =>{
-    setTheme(themes)
-  }
- 
-  useEffect(()=>{
-    handleTheme()
-    console.log(theme)
-  },[])
+  const handleTheme = () => {
+    setTheme(themes);
+  };
+
+  const selectedColor = (item: any) => {
+    setTextClass(item.text);
+    setHoverBgClassName(item.hoverBackground);
+    sethoverTextClassName(item.hoverText);
+
+    themes.forEach((themeItem) => {
+      document.body.classList.remove(themeItem.background);
+    });
+
+    // Ajouter la classe du thème sélectionné
+    document.body.classList.add(item.background);
+    
+  };
+  useEffect(() => {
+    handleTheme();
+    
+  }, []);
   return (
     <>
       <header className="my-2">
         <nav className="flex items-center justify-between">
-          <h1 className={`text-primary font-bold text-4xl ${textClassName}`}>Logo</h1>
+          <h1 className={` font-bold text-4xl ${textClass}`}>
+            Logo
+          </h1>
           <Menu>
             <MenuButton
-              className={`text-primary p-2 rounded-full px-4 font-semibold ${hoverBgClassName}  ${hoverTextClassName} ${textClassName}`}
-              rightIcon={<ChevronDown />}
+              className={` p-2 rounded-full px-4 font-semibold ${hoverBgClassName}  ${hoverTextClassName} ${textClass}`}
             >
               Theme
             </MenuButton>
             <MenuList>
               <div className="flex px-2 justify-start">
-                {theme.map((item, index)=>  <span
-                  className={`w-5 h-5  rounded-full mr-2 hover:cursor-pointer ${item.background}`}
-                  onClick={toggleColorMode}
-                  key={index}
-                ></span>)}
-               
+                {theme.map((item, index) => (
+                  <span
+                    className={`w-5 h-5  rounded-full mr-2 hover:cursor-pointer ${item.background}`}
+                    onClick={() => selectedColor(item)}
+                    key={index}
+                  ></span>
+                ))}
               </div>
             </MenuList>
           </Menu>
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import {ChakraProvider} from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <ChakraProvider>
+  
     <App />
-  </ChakraProvider>
+ 
 );
